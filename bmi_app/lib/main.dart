ffa3bf41636ccbfd76344e8d494cd58a490c50bf
saim-x <x_aci@hotmail.com>
2024-05-13 12:36:45 +0500

    Add BMI calculator UI

--- bmi_app/lib/main.dart
+++ bmi_app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,14 +20,88 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  var wtController = TextEditingController();
+  var ftController = TextEditingController();
+  var inController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text(
+          'BMI CALCULATOR APP',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        elevation: 0, // Remove the shadow
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(10),
+          ),
+        ),
       ),
       body: Center(
-        child: Text('Welcome to my app!'),
+        child: Container(
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+              Text(
+                'BMI',
+                style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.blue,
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              TextField(
+                controller: wtController,
+                decoration: InputDecoration(
+                  hintText: 'Enter Weight in kg',
+                  labelText: 'Weight in kg',
+                  prefixIcon: Icon(Icons.accessibility),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: ftController,
+                decoration: InputDecoration(
+                  hintText: 'Enter Height in ft',
+                  labelText: 'Height in ft',
+                  prefixIcon: Icon(Icons.height),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: inController,
+                decoration: InputDecoration(
+                  labelText: "Height in inches",
+                  hintText: "Enter Height in inches",
+                  prefixIcon: Icon(Icons.height_outlined),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(onPressed: (){}, child: child)
+            ],
+          ),
+        ),
       ),
     );
   }
